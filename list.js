c8e2681195ea6fda058cef4c362a4af3df683dfd
DaemonSpelledWrong <damongself@gmail.com>
2019-02-14 09:20:25 -0500

    Added the empty prepend function for next work session

--- list.js
+++ list.js
@@ -18,5 +18,10 @@ function listToArray(list) {
     return array;
 };
 
+function prepend() {
+    
+};
+
 console.log(arrayToList([10, 20]));
 console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
