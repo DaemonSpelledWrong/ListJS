eaa75b9b2e5f165ab22ed618dbccc76106e2326e
DaemonSpelledWrong <damongself@gmail.com>
2019-02-16 17:15:49 -0500

    Solved prepend function (i think)

--- list.js
+++ list.js
@@ -1,9 +1,9 @@
 
-function arrayToList(input) {
+function arrayToList(arr) {
     let list = null;
 
-    for(let i = input.length - 1; i >= 0; i--){
-        list = {value: input[i], rest: list};
+    for(let i = arr.length - 1; i >= 0; i--){
+        list = {value: arr[i], rest: list};
         
     }
     return list;
@@ -18,10 +18,18 @@ function listToArray(list) {
     return array;
 };
 
-function prepend() {
+function prepend(element, list) {
+    let newList = {value: element, rest: list}
+    
+    return newList;
+};
+
+function nth(list, number) {
+   
     
 };
 
-console.log(arrayToList([10, 20]));
-console.log(listToArray(arrayToList([10, 20, 30])));
-console.log(prepend(10, prepend(20, null)));
+console.log(arrayToList([10, 20])); // → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30]))); // → [10, 20, 30]
+console.log(prepend(10, prepend(20, null))); // → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1)); // → 20
