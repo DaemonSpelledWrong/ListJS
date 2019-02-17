18b7c3be1a2c1696c6b7820f51d648b47513e580
DaemonSpelledWrong <damongself@gmail.com>
2019-02-16 23:32:14 -0500

    Finally solved nth recursivelygit add list.js ! WOO

--- list.js
+++ list.js
@@ -24,21 +24,32 @@ function prepend(element, list) {
     return newList;
 };
 
-function nth(list, index) {
+/*function nth(list, index) {
     let result = [];
+
+    if(list == null || index < 0) {
+        return undefined;
+    };
+
+    for(let node = list; node; node = node.rest) {
+        result.push(node.value);
+    };
+    return result[index];
+};*/
+
+function nth(list, index) {
     if(list == null || index < 0) {
         return undefined;
     };
     if(index == 0) {
-        return list.value;
+        return list.value
     };
     if(index > 0) {
-        for(node = list; node; node = node.rest) {
-            result.push(node.value);
-        };
+       return nth(list.rest, index - 1);
     };
-    return result[index];
 };
+
+
 
 console.log(arrayToList([10, 20])); // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30]))); // → [10, 20, 30]
